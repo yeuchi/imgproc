9b83e9c21f077cce65ebcd2d707b567981d3eb39
yeuchi@gmail.com <yeuchi@gmail.com@605509ea-a62f-11de-8c03-77c0ba871ea3>
2012-08-05 21:25:46 +0000

    remove old comments.

--- trunk/Thining/web/model/model.js
+++ trunk/Thining/web/model/model.js
@@ -1,12 +1,7 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 var model;
 var Model = Backbone.Model.extend({
 
-    // Default attributes for the todo item.
     defaults: function() {
       return {
         thickness: 1,
@@ -15,17 +10,14 @@ var Model = Backbone.Model.extend({
       };
     },
 
-    // Ensure that each todo created has `title`.
     initialize: function() {
        this.thickness = 1;
        this.color = "F00";
        this.listPts = [];
     },
 
-    // Remove this Todo from *localStorage* and delete its view.
     clear: function() {
       this.destroy();
     }
-
 });
 
--- trunk/Thining/web/model/point.js
+++ trunk/Thining/web/model/point.js
@@ -1,7 +1,3 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 var Point = function(x, y) {
    this.x = x;
