964f6d3d6a51d2c02c6e5e38ea86df37d37b5b2a
yeuchi@gmail.com <yeuchi@gmail.com@605509ea-a62f-11de-8c03-77c0ba871ea3>
2012-08-05 21:23:24 +0000

    backbone basic model and view working.

--- trunk/Thining/web/fascade.js
+++ trunk/Thining/web/fascade.js
@@ -2,14 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-$(document).ready(function() {
+//$(document).ready(function() {
+$(function(){
   
-  $("body").load("view/appView.html", onLoadComplete);
-  
-  var onLoadComplete = function() {
-   model = new Model();
-   appView = new AppView();
-  }
-  
+ model = new Model();
+ var appView = new AppView();
+
 });
 
--- trunk/Thining/web/model/model.js
+++ trunk/Thining/web/model/model.js
@@ -3,19 +3,23 @@
  * and open the template in the editor.
  */
 
+var model;
 var Model = Backbone.Model.extend({
 
     // Default attributes for the todo item.
     defaults: function() {
       return {
         thickness: 1,
-        color: F00
+        color: "F00",
+        listPts: []
       };
     },
 
     // Ensure that each todo created has `title`.
     initialize: function() {
-
+       this.thickness = 1;
+       this.color = "F00";
+       this.listPts = [];
     },
 
     // Remove this Todo from *localStorage* and delete its view.
@@ -23,5 +27,5 @@ var Model = Backbone.Model.extend({
       this.destroy();
     }
 
-  });
+});
 
