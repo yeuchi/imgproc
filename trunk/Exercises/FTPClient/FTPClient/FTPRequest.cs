b46169435ede53ac46b1542c8978e3bd0967563c
yeuchi@gmail.com <yeuchi@gmail.com@605509ea-a62f-11de-8c03-77c0ba871ea3>
2012-09-21 14:41:49 +0000

    FTPWebRequest example wrapped as a DLL

---
+++ trunk/Exercises/FTPClient/FTPClient/FTPRequest.cs
@@ -0,0 +1,131 @@
+﻿// Module:      FTPRequest.cs
+//
+// Description: modification of MSDN example for FTP put request
+//              see below link for detail
+// 
+// Reference:   http://msdn.microsoft.com/en-us/library/system.net.ftpwebrequest.aspx#Y4251
+// 
+// Author:      MSDN
+//              modified by CT Yeung
+//
+// Date:        18Sept12
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.IO;
+
+namespace FTPClient
+{
+    public class FTPRequest
+    {
+        public FTPRequest() { }
+
+        public bool upload( string user,    // [in] user name
+                            string pwd,     // [in] password
+                            string filePath,// [in] source file path on source computer 
+                            string uri)     // [in] destination (i.e. ftp://server.com/hello.txt)
+        {
+            ManualResetEvent waitObject;
+            Uri target = new Uri(uri); 
+            FtpState state = new FtpState();
+            FtpWebRequest request = (FtpWebRequest)WebRequest.Create(target);
+            request.Method = WebRequestMethods.Ftp.UploadFile;
+            request.Credentials = new NetworkCredential(user, pwd);
+
+            state.Request = request;
+            state.FileName = filePath;
+
+            // Get the event to wait on.
+            waitObject = state.OperationComplete;
+
+            // Asynchronously get the stream for the file contents.
+            request.BeginGetRequestStream(
+                new AsyncCallback(EndGetStreamCallback),
+                state
+            );
+
+            // Block the current thread until all operations are complete.
+            waitObject.WaitOne();
+
+            // The operations either completed or threw an exception. 
+            if (state.OperationException != null)
+            {
+                throw state.OperationException;
+            }
+            else
+            {
+                Console.WriteLine("The operation completed - {0}", state.StatusDescription);
+                return true;
+            }
+        }
+
+        // The EndGetResponseCallback method   
+        // completes a call to BeginGetResponse. 
+        private static void EndGetResponseCallback(IAsyncResult ar)
+        {
+            FtpState state = (FtpState)ar.AsyncState;
+            FtpWebResponse response = null;
+            try
+            {
+                response = (FtpWebResponse)state.Request.EndGetResponse(ar);
+                response.Close();
+                state.StatusDescription = response.StatusDescription;
+                // Signal the main application thread that  
+                // the operation is complete.
+                state.OperationComplete.Set();
+            }
+            // Return exceptions to the main application thread. 
+            catch (Exception e)
+            {
+                Console.WriteLine("Error getting response.");
+                state.OperationException = e;
+                state.OperationComplete.Set();
+            }
+        }
+
+        private static void EndGetStreamCallback(IAsyncResult ar)
+        {
+            FtpState state = (FtpState)ar.AsyncState;
+
+            Stream requestStream = null;
+            // End the asynchronous call to get the request stream. 
+            try
+            {
+                requestStream = state.Request.EndGetRequestStream(ar);
+                // Copy the file contents to the request stream. 
+                const int bufferLength = 2048;
+                byte[] buffer = new byte[bufferLength];
+                int count = 0;
+                int readBytes = 0;
+                FileStream stream = File.OpenRead(state.FileName);
+                do
+                {
+                    readBytes = stream.Read(buffer, 0, bufferLength);
+                    requestStream.Write(buffer, 0, readBytes);
+                    count += readBytes;
+                }
+                while (readBytes != 0);
+                Console.WriteLine("Writing {0} bytes to the stream.", count);
+                // IMPORTANT: Close the request stream before sending the request.
+                requestStream.Close();
+                // Asynchronously get the response to the upload request.
+                state.Request.BeginGetResponse(
+                    new AsyncCallback(EndGetResponseCallback),
+                    state
+                );
+            }
+            // Return exceptions to the main application thread. 
+            catch (Exception e)
+            {
+                Console.WriteLine("Could not get the request stream.");
+                state.OperationException = e;
+                state.OperationComplete.Set();
+                return;
+            }
+
+        }
+    }
+}
---
+++ trunk/Exercises/FTPClient/FTPClient/FTPstate.cs
@@ -0,0 +1,61 @@
+﻿// Module:      FTPState.cs
+//
+// Description: modification of MSDN example for FTP put request
+//              see below link for detail
+// 
+// Reference:   http://msdn.microsoft.com/en-us/library/system.net.ftpwebrequest.aspx#Y4251
+// 
+// Author:      MSDN
+//              modified by CT Yeung
+//
+// Date:        18Sept12
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+
+namespace FTPClient
+{
+    public class FtpState
+    {
+        private ManualResetEvent wait;
+        private FtpWebRequest request;
+        private string fileName;
+        private Exception operationException = null;
+        string status;
+
+        public FtpState()
+        {
+            wait = new ManualResetEvent(false);
+        }
+
+        public ManualResetEvent OperationComplete
+        {
+            get { return wait; }
+        }
+
+        public FtpWebRequest Request
+        {
+            get { return request; }
+            set { request = value; }
+        }
+
+        public string FileName
+        {
+            get { return fileName; }
+            set { fileName = value; }
+        }
+        public Exception OperationException
+        {
+            get { return operationException; }
+            set { operationException = value; }
+        }
+        public string StatusDescription
+        {
+            get { return status; }
+            set { status = value; }
+        }
+    }
+}
