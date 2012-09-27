390ff6e645486eba58be3e6ac289812ac1a9faae
yeuchi@gmail.com <yeuchi@gmail.com@605509ea-a62f-11de-8c03-77c0ba871ea3>
2012-09-27 01:20:45 +0000

    WPF - C# exercise with STL decoder.
    I have binary decoder working.
    Next to add scaling, rotation then ascii decoder.

---
+++ trunk/DecoderExercise/DecoderExercise/STLParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Media3D;
+
+namespace DecoderExercise
+{
+    public class STLParser
+    {
+        protected byte[] _data;
+        protected int _numTriangles = 0;
+
+        public STLParser(byte[] data)
+        {
+            _data = data;
+        }
+
+        virtual public bool isValid()
+        {
+            return false;
+        }
+
+        virtual public int numTriangles
+        {
+            get
+            {
+                return -1;
+            }
+        }
+
+        virtual public Point3DCollection index(int value)
+        {
+            return null;
+        }
+    }
+}
